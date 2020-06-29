9e54f6f13c9ddd2612c5c7aa859584ec3ba7054d
Calandrinon <georgesut@yahoo.com>
2020-06-29 14:12:36 +0300

    Load quotes only once at the execution of onCreate

--- app/src/main/java/com/example/nietzschepedia/MainActivity.java
+++ app/src/main/java/com/example/nietzschepedia/MainActivity.java
@@ -9,16 +9,12 @@ import java.io.InputStream;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    String[] quotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.displayRandomQuote();
-    }
-
-    public void displayRandomQuote() {
-        String text = "";
 
         try {
             InputStream is = getAssets().open("myquotes.txt");
@@ -26,16 +22,21 @@ public class MainActivity extends AppCompatActivity {
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            text = new String(buffer);
-            String[] parts = text.split("\n");
+            String text = new String(buffer);
+            this.quotes = text.split("\n");
 
-            Random rand = new Random();
-            int random_quote_index = rand.nextInt(parts.length);
-
-            Toast.makeText(MainActivity.this, parts[random_quote_index], Toast.LENGTH_LONG).show();
         } catch (IOException ie) {
-            Toast.makeText(MainActivity.this, "An error occurred!", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "An error occurred in reading the quotes from the file!", Toast.LENGTH_LONG).show();
         }
+
+        displayRandomQuote();
+    }
+
+    public void displayRandomQuote() {
+        Random rand = new Random();
+        int random_quote_index = rand.nextInt(this.quotes.length);
+
+        Toast.makeText(MainActivity.this, this.quotes[random_quote_index], Toast.LENGTH_LONG).show();
     }
 }
 
